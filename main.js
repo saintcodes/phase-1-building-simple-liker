3b905dc08bcacb355e4a7fa2ff7b2c3eed5eae69
Michelle <clairsin@gmail.com>
2022-05-09 10:03:12 -0400

    commit tree 32844c051095b16ee3c2c3d388b7ffccab73294c

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//declares variable 'heart' for heart element on the page
+const hearts = document.querySelectorAll('.like-glyph')
+// console.log(heart)
+
+//adds event listener for 'click' action to each glyph heart on the page and callsback 'clickedHeart' function
+for (const glyph of hearts) {
+  glyph.addEventListener('click', clickedHeart)
+}
+
+function clickedHeart(e) {
+    const heart = e.target
+    console.log(e.target)
+    mimicServerCall()
+    .then(function(success) {
+      alert(success)
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.classList = 'activated-heart'
+      } else if (heart.innerText === FULL_HEART) {
+        heart.innerText = EMPTY_HEART
+        heart.classList = ' '
+        }
+        // heart.classList = 'activated-heart'      
+      })
+      .catch(function (error) {
+        alert(error)
+        let modalDiv = document.querySelector('#modal')
+        modalDiv.classList.remove("hidden")
+        setTimeout(function(){
+          modalDiv.classList.add("hidden")}, 3000)
+      })
+  }
 
 
 
